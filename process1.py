ddf218413c2e9aeca00817b482f61893003bed75
Fernando Ripoll <pipo02mix@gmail.com>
2019-01-04 18:02:32 +0100

    Add severity and description for the alerts

--- process1.py
+++ process1.py
@@ -1,19 +1,32 @@
 import os
 import json
+import datetime
 from elasticsearch import Elasticsearch
 
 
-host, index_agent = os.environ['ELASTICSEARCH_INDEX_URL_AGENT'].rsplit('/', 1)
-_, index_processing = os.environ['ELASTICSEARCH_INDEX_URL_PROCESSING'].rsplit('/', 1)
+host, processing_index = os.environ['ELASTICSEARCH_INDEX_URL_PROCESSING'].rsplit('/', 1)
+_, alert_index = os.environ['ELASTICSEARCH_INDEX_URL_ALERTS'].rsplit('/', 1)
 es = Elasticsearch([host])
 
 
-with open('queries.json') as f:
-  queries = json.load(f)
-  for query in queries['list']:
-    res = es.search(index=index_agent, body={"query": query})
+with open('/usr/src/app/queries.json') as f:
+  rules = json.load(f)
+  for rule in rules['list']:
+    print("Query %s" % rule['query'])
+    query = json.loads(rule['query'])
+    res = es.search(index=processing_index, body={"query": query})
     print("Got %d Hits:" % res['hits']['total'])
 
-    for hit in res['hits']['hits'][0:1]:
-        for item in hit["_source"]["items"]:
-            print(item["name"])
+    if res['hits']['total'] > 0:
+      print("Rule %s:" % rule['name'])
+
+    for doc in res['hits']['hits']:
+      item = doc["_source"]
+      item['description'] = rule['description']
+      item['severity'] = rule['severity']
+      if '_lasttime_processed' not in item:
+        item['_lasttime_processed'] = datetime.datetime.now()
+      if 'kind' in item:
+        esID = item['metadata']['uid'] + '-' + rule['name']
+        res = es.index(index=alert_index, doc_type='_doc', id=esID, body=item)
+        print("  Object %s matched" % item['metadata']['name'])
