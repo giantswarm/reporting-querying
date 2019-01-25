f628595d6616d5cebf31c697ef9176c78d39a660
Fernando Ripoll <pipo02mix@gmail.com>
2019-01-25 12:17:55 +0100

    Remove old alerts and clean code

--- main.py
+++ main.py
@@ -3,39 +3,89 @@ import json
 import datetime
 from elasticsearch import Elasticsearch
 
-
-host, processing_index = os.environ['ELASTICSEARCH_INDEX_URL_PROCESSING'].rsplit('/', 1)
-_, alert_index = os.environ['ELASTICSEARCH_INDEX_URL_ALERTS'].rsplit('/', 1)
+RULES_FILE = './queries.json'
+TODAY_INDEX = datetime.datetime.now().strftime("%y-%m-%d")
+ALERTS_HISTORY_DAYS = os.environ['DAYS_HISTORY']
+host, INDEX_PROCESSOR = os.environ['ELASTICSEARCH_INDEX_URL_PROCESSOR'].rsplit('/', 1)
+_, INDEX_ALERTS = os.environ['ELASTICSEARCH_INDEX_URL_ALERTS'].rsplit('/', 1)
 es = Elasticsearch([host])
 
-
-with open('./queries.json') as f:
-  rules = json.load(f)
-
-  datestring = datetime.datetime.now().strftime("%y-%m-%d")
-  data = {
-    'date': datestring,
+def get_alerts(rules):
+  alerts = {
+    'date': TODAY_INDEX,
     'timestamp': datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ"),
     'items': []
   }
   for rule in rules['list']:
-    query = json.loads(rule['query'])
-    res = es.search(index=processing_index, body={"query": query})
-    print("Got %d alerts for alert %s" % (res['hits']['total'], rule['name']))
+    rule_alerts = get_alert(rule)
+    alerts['items'].append(rule_alerts)
 
-    rule_alerts = {
-      'name': rule['name'],
-      'description': rule['description'],
-      'severity': rule['severity'],
-      'resources': []
-    }
-    for doc in res['hits']['hits']:
-      rule_alerts['resources'].append({
-        'pod': doc["_source"]["metadata"]["name"],
-        'namespace': doc["_source"]["metadata"]["namespace"]
-      })
+  return alerts
 
-    data['items'].append(rule_alerts)
+def get_alert(rule):
+  rule_alerts = {
+    'name': rule['name'],
+    'description': rule['description'],
+    'severity': rule['severity'],
+    'resources': []
+  }
 
-  res = es.index(index=alert_index, doc_type='_doc', id=datestring, body=data)
+  query = json.loads(rule['query'])
+  alerts = search_alerts(query)
+  print("Got %d alerts for alert %s" % (len(alerts), rule['name']))
+  for alert in alerts:
+    rule_alerts['resources'].append({
+      'pod': alert["_source"]["metadata"]["name"],
+      'namespace': alert["_source"]["metadata"]["namespace"]
+    })
+
+  return rule_alerts
+
+def search_alerts(query):
+  res = es.search(index=INDEX_PROCESSOR, body={"query": query})
+
+  return res['hits']['hits']
+
+def save_alerts(alerts):
+  res = es.index(index=INDEX_ALERTS, doc_type='_doc', id=TODAY_INDEX, body=alerts)
   print(res)
+
+def delete_proccesed_index():
+  expr_date = "now-" + ALERTS_HISTORY_DAYS + "d"
+  query = {
+    "query": {
+      "bool": {
+        "must": {
+          "match_all": {}
+        },
+        "filter": {
+          "range": { "@timestamp" : { "lt" : expr_date }}
+        }
+      }
+    }
+  }
+  es.delete_by_query(index=INDEX_PROCESSOR, body={"query": query} ,ignore=[400, 404])
+  print('Old alerts removed')
+
+def delete_old_alerts():
+    es.indices.delete(index=INDEX_PROCESSOR, ignore=[400, 404])
+    print('Processor index deleted')
+
+def exist_processor_index():
+  if not es.indices.exists(index=INDEX_PROCESSOR):
+    print("Index %s not found, skipping the querying" % INDEX_PROCESSOR)
+    quit()
+
+def main():
+  exist_processor_index()
+
+  with open(RULES_FILE) as f:
+    rules = json.load(f)
+
+    alerts = get_alerts(rules)
+    save_alerts(alerts)
+    delete_proccesed_index()
+    delete_old_alerts()
+ 
+if __name__ == "__main__":
+	main()
